c00f35215726d1dfef76fbff9ef7f9e1d8ea5350
Jamie R. Oaks <joaks1@gmail.com>
2019-04-22 13:38:08 -0500

    Adding README and initial script.

---
+++ README.md
@@ -0,0 +1,16 @@
+A simple example of using Python's `random` module.
+
+
+# Acknowledgments
+
+## Support
+This work was made possible by funding provided to [Jamie
+Oaks](http://phyletica.org) from the National Science Foundation (DEB 1656004).
+
+
+# License
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US">Creative Commons Attribution 4.0 International License</a>.
+
+
+
---
+++ permute.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+"A script for randomly permuting input strings."
+
+import sys
+import random
+import argparse
+
+
+def arg_is_positive_int(i):
+    try:
+        if int(i) < 1:
+            raise
+    except:
+        msg = '{0!r} is not a positive integer'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return int(i)
+
+def main_cli():
+    import argparse
+
+    # Create a command-line parser object
+    parser = argparse.ArgumentParser(
+            formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+
+    # Tell the parser what command-line arguments this script can receive
+    parser.add_argument('elements',
+            metavar = 'ELEMENT',
+            nargs = '+', # one or more arguments
+            type = str,
+            help = ('One element to include in the permutation.'))
+    parser.add_argument('--seed',
+            action = 'store',
+            type = arg_is_positive_int,
+            help = ('Seed for random number generator.'))
+
+    args = parser.parse_args()
+
+    # Create an instance of a random number generator
+    random_number_generator = random.Random()
+    if not args.seed:
+        # The user did not provide a seed for the random number generator, so
+        # we have to pick one
+        args.seed = random.randint(1, 999999999)
+    # Seed the random number generator
+    random_number_generator.seed(args.seed)
+
+    # Randomly shuffle the elements provided by the user
+    # The shuffle method modifies the input list, and so returns `None`.
+    random_number_generator.shuffle(args.elements)
+
+    # Loop over the shuffled elements and write them to stdout
+    for el in args.elements:
+        sys.stdout.write("{0}\n".format(el))
+
+
+if __name__ == '__main__':
+    main_cli()
